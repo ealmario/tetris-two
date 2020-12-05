7fb776b39aaa7eb2868bc77d66bc44f35b5e67e8
ealmario <ee.almario@gmail.com>
2020-12-05 16:16:07 +0800

    Create board and piece classes | Add key events

--- js/main.js
+++ js/main.js
@@ -8,3 +8,43 @@ ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 // Scale blocks
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+let board = new Board();
+
+function play() {
+  board.reset();
+  let piece = new Piece(ctx);
+  piece.draw();
+  
+  board.piece = piece;
+}
+
+// Moves
+const moves = {
+  [KEY.LEFT]: p => ({...p, x: p.x - 1}),
+  [KEY.RIGHT]: p => ({...p, x: p.x + 1}),
+  [KEY.DOWN]: p => ({...p, y: p.y + 1}),
+}
+
+// const p = this.moves[event.key](this.piece);
+
+// Keydown Events
+document.addEventListener('keydown', event => {
+  if (moves[event.keyCode]) {
+    // Stop the event from bubbling
+    event.preventDefault();
+
+    // Get new state of piece
+    let p = moves[event.keyCode](board.piece);
+
+    // if (board.valid(p)) {
+      // If the move is valid, move the piece
+      board.piece.move(p);
+
+      // Clear old position before drawing
+      ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
+
+      board.piece.draw();
+    // }
+  }
+})
