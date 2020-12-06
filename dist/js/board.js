bfceee91368461f36f1dd47ff4c0b742f917c962
ealmario <ee.almario@gmail.com>
2020-12-06 15:25:39 +0800

    Add hard drop feature

--- dist/js/board.js
+++ dist/js/board.js
@@ -11,4 +11,28 @@ class Board {
       {length: ROWS}, () => Array(COLS).fill(0)
     );
   }
+
+  insideWalls(x) {
+    return x >= 0 && x < COLS;
+  }
+
+  aboveFloor(y) {
+    return y <= ROWS
+  }
+
+  notOccupied(x,y) {
+    return this.grid[y] && this.grid[y][x] === 0;
+  }
+
+  // Check if the validitiy of position
+  valid(p) {
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+
+        return (value === 0 || (this.insideWalls(x) && this.aboveFloor(y) && this.notOccupied(x, y)));
+      });
+    });
+  }
 }
--- dist/js/main.js
+++ dist/js/main.js
@@ -24,6 +24,7 @@ const moves = {
   [KEY.LEFT]: p => ({...p, x: p.x - 1}),
   [KEY.RIGHT]: p => ({...p, x: p.x + 1}),
   [KEY.DOWN]: p => ({...p, y: p.y + 1}),
+  [KEY.SPACE]: p => ({...p, y: p.y + 1})
 }
 
 // const p = this.moves[event.key](this.piece);
@@ -31,20 +32,21 @@ const moves = {
 // Keydown Events
 document.addEventListener('keydown', event => {
   if (moves[event.keyCode]) {
-    // Stop the event from bubbling
     event.preventDefault();
 
-    // Get new state of piece
+    // Get new state
     let p = moves[event.keyCode](board.piece);
-
-    // if (board.valid(p)) {
-      // If the move is valid, move the piece
+    if (event.keyCode === KEY.SPACE) {
+      // Hard drop
+      while (board.valid(p)) {
+        board.piece.move(p);
+        p = moves[KEY.DOWN](board.piece);
+      }      
+    } else if (board.valid(p)) {
       board.piece.move(p);
+    }
 
-      // Clear old position before drawing
-      ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
-
-      board.piece.draw();
-    // }
+    ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
+    board.piece.draw();
   }
 })
